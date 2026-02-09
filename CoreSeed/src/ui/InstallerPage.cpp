34bd91e6f129f2904034da88a12ecbd11c852516
CodeAkrobat <lead@neurabsd.org>
2026-02-09 21:01:46 +0100

    RECOVERY: Restore modular source tree from local VM

---
+++ CoreSeed/src/ui/InstallerPage.cpp
@@ -0,0 +1,8 @@
+/* $NeuraBSD: CoreSeed/src/ui/InstallerPage.cpp, v 1.1 2026/02/09 codeakrobat Exp $ */
+#include "InstallerPage.hpp"
+
+/* * DE: Diese Datei ist notwendig, damit der Qt-MOC-Compiler 
+ * die Meta-Informationen für die Basisklasse korrekt generiert.
+ * EN: This file is necessary for the Qt MOC compiler to 
+ * correctly generate meta-information for the base class.
+ */
